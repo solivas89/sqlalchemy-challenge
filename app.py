8ec6e78584761209b0abd5876e4eb858f59869cc
solivas89 <sal.olivas1989@gmail.com>
2020-08-20 17:28:12 -0700

    08/20/2020 Part 1 Commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+from flask import Flask, jsonify
+
